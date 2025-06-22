4427084a3968f2c656b967b4f7c9dff62c414197
WINNVYN08 <152044695+WINNVYN08@users.noreply.github.com>
2025-06-23 09:55:24 +1200

    Create start_menu.gd

---
+++ 12dt1-assementnt-project/scripts/start_menu.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+	pass # Replace with function body.
+
+
+func _on_credits_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_new_game_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_continue_pressed() -> void:
+	pass # Replace with function body.
