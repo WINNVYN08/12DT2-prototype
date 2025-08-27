8f160fb7f7c1edbefc166700a9d0f85dcfa35850
WINNVYN08 <152044695+WINNVYN08@users.noreply.github.com>
2025-08-27 18:30:57 +1200

    Update start_game_button.gd

--- 12dt1-assementnt-project/scripts/start_game_button.gd
+++ 12dt1-assementnt-project/scripts/start_game_button.gd
@@ -1,6 +1,14 @@
 extends Node2D
 
+@onready var path_follow_1 : PathFollow2D = $Path2D/PathFollow1
+@onready var path_follow_2 : PathFollow2D = $Path2D/PathFollow2
+
+@export var speed = 2
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/start_menu.tscn")
 	pass # Replace with function body.
+
+func _process(delta: float) -> void:
+	path_follow_1.progress += speed
+	path_follow_2.progress += speed
