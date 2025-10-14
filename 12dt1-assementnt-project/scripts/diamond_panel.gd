b806daf356110a09f5838c276b0e75b3dc2c3969
WINNVYN08 <152044695+WINNVYN08@users.noreply.github.com>
2025-10-15 09:10:37 +1300

    2

--- 12dt1-assementnt-project/scripts/diamond_panel.gd
+++ 12dt1-assementnt-project/scripts/diamond_panel.gd
@@ -1,9 +1,9 @@
 extends Panel
 
-@onready var sprite = $Sprite2D
+@export var sprite: Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
