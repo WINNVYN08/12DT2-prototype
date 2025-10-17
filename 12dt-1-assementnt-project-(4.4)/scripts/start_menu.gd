666b2bd8e8d7486b2fac6022c3b37042e10fb4a3
WINNVYN08 <152044695+WINNVYN08@users.noreply.github.com>
2025-10-17 23:31:23 +1300

    s

--- 12dt-1-assementnt-project-(4.4)/scripts/start_menu.gd
+++ 12dt-1-assementnt-project-(4.4)/scripts/start_menu.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
 func _process(delta: float) -> void:
-	await get_tree().create_timer(7).timeout
-	get_tree().change_scene_to_file("res://scenes/act_10.tscn")
+	await get_tree().create_timer(4).timeout
+	get_tree().change_scene_to_file("res://scenes/act_1.tscn")
 	pass
